6a6bcb3d2c1daa21f6ae606c744f5b8067106a90
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-06-26 11:40:05 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
+--[[ if table.find({Enum.Platform.IOS, Enum.Platform.Android}, game:GetService("UserInputService"):GetPlatform()) then
+    
+end ]]
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
