dc1813ba042f7af61584bccfa914b7ce806db1b7
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-06-30 21:03:31 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,5 @@
     
 end ]]
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
+--loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9a60e45955b3c39384472a52a19ace2d.lua"))() -- temp fix until federal verifies
