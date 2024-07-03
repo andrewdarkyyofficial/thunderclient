5a5fe3aae6a85e88c79f32b0e62d512cfec42004
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-07-03 13:40:13 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,1 @@
---[[ if table.find({Enum.Platform.IOS, Enum.Platform.Android}, game:GetService("UserInputService"):GetPlatform()) then
-    
-end ]]
-
---loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9a60e45955b3c39384472a52a19ace2d.lua"))() -- temp fix until federal verifies
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
