d815988cfb8af20b71a2ad350fd562c330146534
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-07-15 17:27:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/K8gdWHthVw")
-game.Players.LocalPlayer:Kick("Thunder Client will be back on the 16th of July. Join new server: https://discord.gg/K8gdWHthVw (already copied)")
+game.Players.LocalPlayer:Kick("Thunder Client will be back soon. Join new server: https://discord.gg/K8gdWHthVw (already copied)")
