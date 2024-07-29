1808227ae92c3f8bbd692d4b7c8a944fed2cdafe
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-07-29 10:46:07 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/39177ad3a992256c5b7cf1ccf23adae4.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
