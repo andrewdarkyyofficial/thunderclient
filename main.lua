1aed51478ed2fb1e2c2c6ccc9aa76ae8fae85321
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-07-29 11:37:31 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
+--[[ if identifyexecutor() == "Wave" then
+    while true do end
+end ]]
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/verified/dca3e69649ed196af0ac6577f743a0ae.lua"))()
