559469dfff932c214a1859a0e2525ffb5d8b41ce
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-10-30 18:51:17 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/83ad6a18b56205830d2a1f7266b5ca10.lua'))();
+loadstring(game:HttpGet('https://cdn.luarmor.net/verified/thunderclient.lua'))();
