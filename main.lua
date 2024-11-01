1f68a636a172b08f53dcb1c9a183fcfaf497c6b7
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-11-01 17:49:58 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,5 @@
---// loadstring(game:HttpGet('https://cdn.luarmor.net/verified/thunderclient.lua'))();
-loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/83ad6a18b56205830d2a1f7266b5ca10.lua'))();
+if identifyexecutor() == 'Solara' then
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))();
+else
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/83ad6a18b56205830d2a1f7266b5ca10.lua'))();
+end;
