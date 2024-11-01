8b74ced1720602e74ced90af7c07f7d458407672
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-11-01 17:59:35 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
-if identifyexecutor() == 'Solara' then
+local executorname = getexecutorname();
+
+if executorname == 'Solara' or executorname == 'Xeno' then
     loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))();
 else
     loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/83ad6a18b56205830d2a1f7266b5ca10.lua'))();
