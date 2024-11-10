648433a4f6ae13396ff49881393de299b9268bf1
andrewdarkyyofficial <ovsfandre@gmail.com>
2024-11-10 10:48:14 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-local executorname = getexecutorname();
+local executorname = getexecutorname()
 
 if executorname == 'Solara' or executorname == 'Rebel' or executorname == 'Xeno' then
-    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))();
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))()
 else
-    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/83ad6a18b56205830d2a1f7266b5ca10.lua'))();
-end;
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/83ad6a18b56205830d2a1f7266b5ca10.lua'))()
+end
