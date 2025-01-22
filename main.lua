0cc278c8c427b2c03e6a2c4e9068bbfb9ab3b8ff
andrewdarkyyofficial <ovsfandre@gmail.com>
2025-01-22 20:26:13 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,13 @@
-local executorname = getexecutorname()
+local unsupported = not request or ({
+    Unknown = true,
+    Delta = true,
+    Fluxus = true,
+    Rebel = true,
+    Solara = true,
+    Xeno = true
+})[identifyexecutor and identifyexecutor() or 'Unknown']
 
-if executorname == 'Xeno' or executorname == 'Fluxus' or executorname == 'Solara' or executorname == 'Rebel' or executorname == 'Delta' then
+if unsupported then
     loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'))()
 else
     loadstring(game:HttpGet('https://cdn.luarmor.net/verified/thunderclient.lua'))()
