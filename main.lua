206cb0b0feabfba39fef616a9bd0a986a5063f74
andrewdarkyyofficial <ovsfandre@gmail.com>
2025-01-22 20:51:25 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local url = 'https://cdn.luarmor.net/verified/thunderclient.lua'
 
-if not hookfunction or not isfunctionhooked or not clonefunction then
+if not clonefunction or not hookfunction or not isfunctionhooked then
     url = 'https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'
 end
 
