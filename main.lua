bc3039bc4dafb9924a7b30c75e669e53edfc09cb
andrewdarkyyofficial <ovsfandre@gmail.com>
2025-03-16 17:08:06 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,12 +2,10 @@ if not loadstring or not request then
     return game:GetService('Players').LocalPlayer:Kick('Your executor is not supported.')
 end
 
-local url = 'https://cdn.luarmor.net/verified/thunderclient.lua'
+local url = 'https://api.luarmor.net/files/v3/loaders/d911fd2915ce2e7fc4a104a28d00c9b4.lua'
 
 if not clonefunction or not hookfunction or not isfunctionhooked then
     url = 'https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'
 end
-
---// discord.gg/K8gdWHthVw
 
 loadstring(game:HttpGet(url))()
