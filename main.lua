0eb23cc372be0a1d4489a414577ef89b351ed403
andrewdarkyyofficial <ovsfandre@gmail.com>
2025-04-20 15:43:56 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 task.delay(1, function()
-    local cloneref = cloneref or function(...)
-        return ...
-    end
+	local cloneref = cloneref or function(...)
+		return ...
+	end
 
 	local tweenService = cloneref(game:GetService('TweenService'))
 
@@ -104,7 +104,7 @@ task.delay(1, function()
 	instances.TextLabel3.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 and instances.TextLabel3.Text == 'Learn More' then
 			setclipboard('https://bloxproducts.com/r/1141035554239160360')
-			
+
 			instances.TextLabel3.Text = 'Link Copied!'
 
 			tweenService:Create(instances.TextLabel3, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out, 0, true), {
@@ -149,7 +149,7 @@ end)
 local url = 'https://api.luarmor.net/files/v3/loaders/d911fd2915ce2e7fc4a104a28d00c9b4.lua'
 
 if not clonefunction or not hookfunction or not isfunctionhooked then
-    url = 'https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'
+	url = 'https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'
 end
 
 loadstring(game:HttpGet(url))()
