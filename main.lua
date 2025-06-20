f6c985d834f1a928bb9da0bdf5088d1484bde335
andrewdarkyyofficial <ovsfandre@gmail.com>
2025-06-20 19:00:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -146,10 +146,10 @@ task.delay(1, function()
 	end
 end)
 
-local url = 'https://api.luarmor.net/files/v3/loaders/d911fd2915ce2e7fc4a104a28d00c9b4.lua'
+local url = 'https://api.luarmor.net/files/v4/loaders/d911fd2915ce2e7fc4a104a28d00c9b4.lua'
 
 if not clonefunction or not hookfunction or not isfunctionhooked then
-	url = 'https://api.luarmor.net/files/v3/loaders/b95e8fecdf824e41f4a030044b055add.lua'
+	url = 'https://api.luarmor.net/files/v4/loaders/b95e8fecdf824e41f4a030044b055add.lua'
 end
 
 loadstring(game:HttpGet(url))()
